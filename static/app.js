55f69d094cf4c005860048c795432f5dbe51d895
Clayfranklin <claystudydata@gmail.com>
2019-12-11 14:05:32 -0600

    returns big sql pull, parsing with d3 for plots

--- static/app.js
+++ static/app.js
@@ -1,64 +1,54 @@
 // // REQUEST THE DATA
-// function build_data(skater) {
 
-//   // @TODO: Complete the following function that builds the metadata panel
-//   const url = "/games_played/";
-//   let selector = d3.select("#skater_games");
-//   selector.html("");
-//   d3.json(url).then((skater) => {
-//     Object.entries(skater).forEach(([key, value]) => {
-//       selector.append("h6")
-//         .text(key + ": " + value);
-
-//     });
-//   });
-// }
-const url = "http://127.0.0.1:5000/games_played";
+const url = "http://127.0.0.1:5000/bday_group1";
 d3.json(url).then(function(data) {
     console.log(data)
+   
+    var bdays = d3.values(data["8448208"][5])
+    console.log(bdays)
+    var games = d3.values(data["8448208"][6])
+    console.log(games)
+    stats = Object.entries(data).forEach(([key, value]) => {
+      country = value[2]
+      bday = value[1]
+      console.log(country)
+      console.log(bday)
+      ; 
+    });
     // Grab values from the response json object to build the plots
-    var player = data.key;
-    var games = data.value;
 
     var trace1 = {
       type: "scatter",
       mode: "lines",
-      x: player,
-      y: games,
+      x: country,
+      y: bday,
       line: {
         color: "#17BECF"
       }
     };
 
-    var data = [trace1];
+    var graph = [trace1];
 
-    // var layout = {
-    //   title: `this`,
-    //   xaxis: {
-    //     range: [startDate, endDate],
-    //     type: "date"
-    //   },
-    //   yaxis: {
-    //     autorange: true,
-    //     type: "linear"
-    //   }
-    // };
+  //   // var layout = {
+  //   //   title: `this`,
+  //   //   xaxis: {
+  //   //     range: [startDate, endDate],
+  //   //     type: "date"
+  //   //   },
+  //   //   yaxis: {
+  //   //     autorange: true,
+  //   //     type: "linear"
+  //   //   }
+  //   // };
 
-    Plotly.newPlot("plot", data);
+    Plotly.newPlot("plot", graph);
 
   });
-  const url2 = "http://127.0.0.1:5000/bday_group1";
-  d3.json(url2).then(function(data2) {
-      console.log(data2)
-        
 
-    let trace1 = {
-      labels : data2,
-      values : data2,
-      hovertext: data2,
-      type: "pie"
-    };
+  const url2 = "http://127.0.0.1:5000/games_played";
+  d3.json(url2).then(function(data) {
+      console.log(data)
 
-    let pie_data = [trace1];
-    Plotly.newPlot("pie", pie_data);
+      var Jan_March = d3.values(data)
+      console.log(Jan_March)
   });
